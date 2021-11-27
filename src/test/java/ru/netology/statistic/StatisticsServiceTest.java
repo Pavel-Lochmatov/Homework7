2a58d38c204a4ea8e396edf479f82d4f3b66cfe6
Pavel-Lokhmatov <hlest.ivsvnyby@gmail.com>
2021-11-27 18:37:58 +0400

    fail StatisticsServiceTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {1, 5, 18, 4, 5, 3, 8, 6, 18, 11, 12};
-        long expected = 18;
+        long expected = 17;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
